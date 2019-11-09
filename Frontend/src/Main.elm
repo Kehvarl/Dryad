e7e387704c1d966b3e9548286a660b7594d4c987
Kehvarl <kehvarl@kehvarl.com>
2019-11-08 22:23:59 -0700

    Display Hello World

--- Frontend/src/Main.elm
+++ Frontend/src/Main.elm
@@ -2,6 +2,8 @@ module Main exposing (..)
 
 import Browser
 import Html exposing (..)
+import Http
+import Json.Decode exposing (Decoder, field, list, string)
 
 
 
@@ -20,24 +22,58 @@ main =
 
 init : () -> ( Model, Cmd Msg )
 init () =
-    ( { value = "Hello World." }, Cmd.none )
+    ( Loading, getChatPosts )
 
 
 
 --UPDATE
 
 
-type alias Model =
-    { value : String }
+type Model
+    = Failure
+    | Loading
+    | Success Chat
 
 
 type Msg
     = NoOp
+    | GotChat (Result Http.Error Chat)
+
+
+type alias Chat =
+    { room : String
+    , posts : List String
+    }
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update _ model =
-    ( model, Cmd.none )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+        GotChat result ->
+            case result of
+                Ok chat ->
+                    ( Success chat, Cmd.none )
+
+                Err _ ->
+                    ( Failure, Cmd.none )
+
+
+getChatPosts : Cmd Msg
+getChatPosts =
+    Http.get
+        { url = "http://localhost/Main.php"
+        , expect = Http.expectJson GotChat chatDecoder
+        }
+
+
+chatDecoder : Decoder Chat
+chatDecoder =
+    Json.Decode.map2 Chat
+        (field "room" string)
+        (field "posts" (list string))
 
 
 
@@ -46,4 +82,17 @@ update _ model =
 
 view : Model -> Browser.Document Msg
 view model =
-    { title = "", body = [ text model.value ] }
+    case model of
+        Failure ->
+            { title = "", body = [ text "ERROR" ] }
+
+        Loading ->
+            { title = "", body = [ text "Loading" ] }
+
+        Success value ->
+            { title = value.room, body = [ Html.ul [] <| List.map viewPosts value.posts ] }
+
+
+viewPosts : String -> Html Msg
+viewPosts post =
+    Html.li [] [ text post ]
